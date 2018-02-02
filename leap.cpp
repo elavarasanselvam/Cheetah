baac79ab869ed07049208bd01ba82762f519d766
elavarasanselvam <34589746+elavarasanselvam@users.noreply.github.com>
2018-02-02 15:46:50 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+    if (year % 4 == 0)
+    {
+       cout<<"yes";
+    }
+    else
+        cout<< "not";
+
+    return 0;
+}
