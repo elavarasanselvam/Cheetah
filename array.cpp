138a917e4b09ddd12fd271b93acd09dfd87b24e3
elavarasanselvam <34589746+elavarasanselvam@users.noreply.github.com>
2018-02-06 13:17:33 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main()
 {
     int sum=0;
-    int n,k,a[5],i;
-    cin>>n>>k;
-    for(i=0;i<n;i++)
+    int p,q,a[5],i;
+    cin>>p>>q;
+    for(i=0;i<p;i++)
     {
         cin>>a[i];
     }
-    for(i=0;i<k;i++)
+    for(i=0;i<q;i++)
     {
         sum=sum+a[i];
     }
